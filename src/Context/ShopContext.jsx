99af18d58e77ba353bfee500f3affcaaa0256eed
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2024-03-07 19:13:49 +0000

    context and category

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,17 +1,15 @@
 import { createContext } from "react";
 import all_products from "../assets/allproducts";
 
-export const ShopContext = createContext(null)
+export const ShopContext = createContext(null);
 
-
-const ShopContextProvider =(props)=>{
-    const contextValue ={all_products}
-    return(
-        <ShopContext.Provider value={createContext}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
-
+const ShopContextProvider = (props) => {
+  const contextValue = { all_products };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,4 +1,5 @@
 import all_products from "../assets/allproducts"
+import Item from "../components/Item"
 
 const Category = ({ category, banner }) => {
     return (
@@ -10,16 +11,21 @@ const Category = ({ category, banner }) => {
                 <h5>Showing 1-12 out of 36 products</h5>
             </div>
             <div>
-                {all_products.map((item) => (
-                    <Item
-                        key={item.id}
-                        id={item.id}
-                        name={item.name}
-                        category={item.category}
-                        price={item.price}
-                        rating={item.rating}
-                    />
-                ))}
+                {all_products.map((item) => {
+                    if(category === item.category){
+                        return(
+                            <Item
+                            key={item.id}
+                            id={item.id}
+                            name={item.name}
+                            category={item.category}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                        )
+                    }
+                 
+})}
             </div>
         </>
     )
