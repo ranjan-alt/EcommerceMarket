0b703b83d77d184574626db7f612821f3f194784
Ranjan <ranjan.kumar@publicissapient.com>
2024-03-08 00:37:35 +0530

    category

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,13 +1,26 @@
-const Category =({category, banner})=>{
-    return(
+import all_products from "../assets/allproducts"
+
+const Category = ({ category, banner }) => {
+    return (
         <>
-        <div>
-            <img src={banner} alt="" />
-        </div>
-        <div>
-            <h5>Showing 1-12 out of 36 products</h5>
-        </div>
-        
+            <div>
+                <img src={banner} alt="" />
+            </div>
+            <div>
+                <h5>Showing 1-12 out of 36 products</h5>
+            </div>
+            <div>
+                {all_products.map((item) => (
+                    <Item
+                        key={item.id}
+                        id={item.id}
+                        name={item.name}
+                        category={item.category}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                ))}
+            </div>
         </>
     )
 }
